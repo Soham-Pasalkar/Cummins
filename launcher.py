d573f097e33be1e045b7b478ca02466135ee5fd2
Soham Pasalkar <sohampasalkar07@gmail.com>
2026-03-30 15:14:13 +0530

    attempt 3

--- launcher.py
+++ launcher.py
@@ -1,5 +1,9 @@
 import subprocess
 import sys
 
-# Start Streamlit and let it handle browser opening
-subprocess.run([sys.executable, "-m", "streamlit", "run", "app.py"])
+subprocess.run([
+    sys.executable,
+    "-m", "streamlit",
+    "run", "app.py",
+    "--server.headless=false"
+])
