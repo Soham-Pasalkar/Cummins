11d57f572d71c6c7797017a1ebcdac24dd578051
Soham Pasalkar <sohampasalkar07@gmail.com>
2026-03-30 15:26:12 +0530

    update 4

--- launcher.py
+++ launcher.py
@@ -1,9 +1,12 @@
 import subprocess
 import sys
+import os
 
-subprocess.run([
-    sys.executable,
-    "-m", "streamlit",
-    "run", "app.py",
-    "--server.headless=false"
-])
+# Get correct path inside EXE
+base_path = getattr(sys, '_MEIPASS', os.path.dirname(__file__))
+app_path = os.path.join(base_path, "app.py")
+
+print("Running from:", base_path)
+print("App path:", app_path)
+
+subprocess.run([sys.executable, "-m", "streamlit", "run", app_path])
